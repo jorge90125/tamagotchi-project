1b2c22a004c74a79e48e7d41b212ec955f2e490d
jorge90125 <jorge.gonzalez7@g.austincc.edu>
2022-07-08 23:26:43 -0500

    Made changes to functions/animations/dead sequence

--- js/app.js
+++ js/app.js
@@ -18,28 +18,28 @@ class Character {
         this.age = 0
     }
     feed() {
-        if (this.hunger >= 10) {
+        if (this.hunger <= 0) {
             return
         } else {
-            this.hunger ++
+            this.hunger --
             hunger.innerHTML = this.hunger
         }
     }
     sleep() {
-        if (this.sleepiness >= 10) {
+        if (this.sleepiness <= 0) {
             return
         } else {
             document.body.style.backgroundImage = `url('../tamagotchi-project/images/windows-bg-night.jpeg')`
-            this.sleepiness ++
+            this.sleepiness --
             sleepiness.innerHTML = this.sleepiness
             setTimeout(() => {document.body.style.backgroundImage = `url('../tamagotchi-project/images/windows-bg.jpeg')`}, 2000)
         }
     }
     play() {
-        if (this.boredom >= 10) {
+        if (this.boredom <= 0) {
             return
         } else {
-            this.boredom ++
+            this.boredom --
             boredom.innerHTML = this.boredom
         }
     }
@@ -53,47 +53,47 @@ const playBtn = document.querySelector(`#playBtn`)
 //aging function
 const agePet = (characterToAge) => {
     if (characterToAge.age === 14) {
-        characterImg.src = `images/stg-4.jpg`
+        characterImg.src = `images/stg-4.png`
         characterToAge.age ++
         age.innerHTML = characterToAge.age
         characterName.innerHTML = `LVL 4 ${characterToAge.name}`
-        characterImg.style.width = `300px`
+        characterImg.style.width = `350px`
     }
     if (characterToAge.age === 9) {
         characterImg.src = `images/stg-3.png`
         characterToAge.age ++
         age.innerHTML = characterToAge.age
         characterName.innerHTML = `LVL 3 ${characterToAge.name}`
-        characterImg.style.width = `200px`
+        characterImg.style.width = `250px`
     }
     if (characterToAge.age === 4) {
         characterImg.src = `images/stg-2.png`
         characterToAge.age ++
         age.innerHTML = characterToAge.age
         characterName.innerHTML = `LVL 2 ${characterToAge.name}`
-        characterImg.style.width = `150px`
+        characterImg.style.width = `250px`
     } else {
         characterToAge.age ++
         age.innerHTML = characterToAge.age
     }
 }
 
-//stats decrease over time function
-const statsDec = (nameOfChar) => {
-    nameOfChar.hunger --
+//stats increase over time function
+const statsInc = (nameOfChar) => {
+    nameOfChar.hunger ++
     hunger.innerHTML = nameOfChar.hunger
-    nameOfChar.sleepiness --
+    nameOfChar.sleepiness ++
     sleepiness.innerHTML = nameOfChar.sleepiness
-    nameOfChar.boredom --
+    nameOfChar.boredom ++
     boredom.innerHTML = nameOfChar.boredom
 }
 
 //game function
 const runGame = () => {
-    // alert(`Welcome to the most epic Tamagotchi game!`)
-    // alert(`After entering your pet's name, a timer will begin that slowly lowers your stats as well as slowly aging your pet.`)
-    // alert(`Once a stat reaches 0, it's game over! Click the different activity buttons to up your pet's stats and stay alive.`)
-    // alert(`Eventually, when your pet is old enough, it will evolve. Try to make it to the last evolution!`)
+    alert(`Welcome to the most epic Tamagotchi game!`)
+    alert(`After entering your pet's name, a timer will begin that slowly raises your stats as well as slowly aging your pet.`)
+    alert(`Once a stat reaches 10, it's game over! Click the different activity buttons to keep your  pet's stats down and stay alive.`)
+    alert(`Eventually, when your pet is old enough, it will evolve. Try to make it to the last evolution!`)
     let name = prompt(`Choose your pet's name!`, `Turdle`)
     const character = new Character(`${name}`)
     hunger.innerHTML = character.hunger
@@ -106,12 +106,17 @@ const runGame = () => {
     feedBtn.addEventListener(`click`, () => {character.feed()})
     sleepBtn.addEventListener(`click`, () => {character.sleep()})
     playBtn.addEventListener(`click`, () => {character.play()})
-    setInterval(() => {agePet(character)}, 5000)
-    setInterval(() => {statsDec(character)}, 3000)
-    setInterval(() => {
-        if (character.hunger === 0 || character.sleepiness === 0 || character.boredom === 0) {
-        alert(`Boooooo! You have failed as a caretaker. Your pet has moved on to the next plane of existence. This page  will refresh and you can try again.`)
-        location.reload()
+    const ageInterval = setInterval(() => {agePet(character)}, 2000)
+    const statInterval = setInterval(() => {statsInc(character)}, 3000)
+    const deadInterval = setInterval(() => {
+        if (character.hunger === 10 || character.sleepiness === 10 || character.boredom === 10) {
+            clearInterval(ageInterval)
+            clearInterval(statInterval)
+            characterImg.style.animationName = `paused`
+            characterImg.src = `images/tombstone.png`
+            characterImg.style.width = `200px`
+            alert(`Boooooo! You have failed as a caretaker. Your pet has moved on to the next plane of existence. Press the "Begin Game" button to try again!`)
+            clearInterval(deadInterval)
         }
     }, 1000)
 }
